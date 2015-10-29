986ec5a497cc4f01830f31e41a011e2fab9ed051
Stephanie Nemeth <traumverloren@gmail.com>
2015-10-29 18:38:21 +0100

    adding more functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 angular.module('flapperNews', [])
 .controller('MainCtrl', [
   '$scope',
-  function($scope){
+  function($scope) {
     $scope.test = 'Hello world!';
     $scope.posts = [
       {title: 'post 1', upvotes: 5},
@@ -9,5 +9,14 @@ angular.module('flapperNews', [])
       {title: 'post 3', upvotes: 24},
       {title: 'post 4', upvotes: 3},
       {title: 'post 5', upvotes: 6},
-    ]
+    ];
+    $scope.addPost = function() {
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
   }]);
