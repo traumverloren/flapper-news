9b8059bd306752fd16a59dc64c94aee7a204d1b5
Stephanie Nemeth <traumverloren@gmail.com>
2015-10-30 10:22:10 +0100

    add routing, posts, home, comments

--- app.js
+++ app.js
@@ -1,22 +1,71 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      })
+
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });
+    $urlRouterProvider.otherwise('home');
+  }])
+.factory('posts', [function() {
+  var o = {
+    posts: []
+  };
+  return o;
+}])
 .controller('MainCtrl', [
   '$scope',
-  function($scope) {
-    $scope.test = 'Hello world!';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 24},
-      {title: 'post 4', upvotes: 3},
-      {title: 'post 5', upvotes: 6},
-    ];
+  'posts',
+  function($scope, posts) {
+    $scope.posts = posts.posts;
     $scope.addPost = function() {
       if(!$scope.title || $scope.title === '') { return; }
-      $scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
+
       $scope.title = '';
       $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
     };
+  }])
+.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts) {
+    $scope.post = posts.posts[$stateParams.id];
+
+  $scope.addComment = function() {
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+
+  $scope.incrementUpvotes = function(comment) {
+    comment.upvotes += 1;
+  };
   }]);
